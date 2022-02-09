b9333441cfc809760ecbd9260815b7e5103ac3ed
Arthurjim <arthurjim013@gmail.com>
2022-02-09 17:13:39 -0600

    Created componets for pokeon images and pokemons options

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="pokemon-container">
+        <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="pokemon"
+            class="hidden-pokemon"
+        />
+        <!-- <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="pokemon"
+            class="fade-in"
+        /> -->
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.pokemon-container {
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+img {
+    position: absolute;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>¿Quien es este pókemon?</h2>
+    <PokemonPicture />
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+export default {
+    name:'PokemonPage',
+    components:{
+      PokemonOptions,
+      PokemonPicture
+    }
+}
+</script>
+
+<style>
+
+</style>
