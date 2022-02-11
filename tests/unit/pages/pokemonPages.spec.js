e87721ec6b306858435021390582c0292ec0194a
Arthurjim <arthurjim013@gmail.com>
2022-02-11 12:48:19 -0600

    Test in component pokemonPages

--- tests/unit/pages/pokemonPages.spec.js
+++ tests/unit/pages/pokemonPages.spec.js
@@ -1,5 +1,8 @@
 import { mount, shallowMount } from "@vue/test-utils";
 import PokemonPage from '@/pages/PokemonPage'
+
+
+import { pokemonsMock } from "../mocks/pokemons.mock";
 describe("Pokemon pages", () => {
     let wrapper;
     // let mixPokemonArray;
@@ -17,4 +20,56 @@ describe("Pokemon pages", () => {
         shallowMount(PokemonPage) 
         expect(mixPokemonArray).toHaveBeenCalled()
      })
+
+     test('debe de hacer match con el snapshor cuando cargan los elemenos', () => { 
+         const wrapper = mount(PokemonPage,{
+             data(){
+                return {
+                    pokemonsArr: pokemonsMock,
+                    pokemon: pokemonsMock[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: "",
+                    pokemonSelected:null
+                };
+             }
+         })
+
+         expect(wrapper.html()).toMatchSnapshot()
+      })
+      test('debe de mostrar los componentes de PokemonPicture y PokemonOptions', () => {
+
+        const wrapper = shallowMount(PokemonPage,{
+            data(){
+               return {
+                   pokemonsArr: pokemonsMock,
+                   pokemon: pokemonsMock[0],
+                   showPokemon: false,
+                   showAnswer: false,
+                   message: "",
+                   pokemonSelected:null
+               };
+            }
+        })
+        expect(wrapper.html()).toMatchSnapshot()
+
+        const pokemonPicture = wrapper.find('pokemon-picture-stub')
+        const pokemonOptions =wrapper.find('pokemon-options-stub')
+        
+       expect(pokemonPicture.exists()).toBeTruthy()
+       expect(pokemonOptions.exists()).toBeTruthy()
+
+       expect(pokemonPicture.attributes('pokemonid')).toBe('1')
+       expect(pokemonOptions.exists('pokemons')).toBe(true)
+
+
+        // expect(wrapper.findComponent(PokemonPicture).exists()).toBe(true);
+        // expect(wrapper.find('PokemonOptions').exists()).toBe(true);
+
+        // console.log(wrapper.element())
+        // PokemonPicture
+        // PokemonPicture
+
+        // PokemonPicture pokemonId = 5
+      })
 });
